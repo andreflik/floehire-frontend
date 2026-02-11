541d4029b6d094e4fcfae638efb58115c708ffd2
Andre Fagundes <andreflik@gmail.com>
2026-02-11 17:42:09 -0300

    feat: Projeto base subindo para develop

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "floe-yellow": "#FFD700",
+      },
+    },
+  },
+  plugins: [],
+};
